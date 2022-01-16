d95b092e9182a48920ee2098c7775ac3b0cb3311
Abhishek Shukla <50260867+abhishek-shukla21@users.noreply.github.com>
2022-01-16 10:28:13 +0530

    Create WAP to create a simple calculator using if-else.c

---
+++ WAP to create a simple calculator using if-else.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+int main()
+{
+
+    char opt;
+    int num1, num2;
+    float res;
+    printf (" Select an operator +, -, *, / \n ");
+    scanf ("%c", &opt);
+    printf (" Enter the first number: ");
+    scanf(" %d", &num1);
+    printf (" Enter the second number: ");
+    scanf (" %d", &num2);
+
+    if (opt == '+')
+    {
+        res = num1 + num2;
+        printf ("%f",res);
+    }
+
+    else if (opt == '-')
+    {
+        res = num1 - num2;
+        printf (" %f", res);
+    }
+
+    else if (opt == '*')
+    {
+        res = num1 * num2;
+        printf ("%f", res);
+    }
+
+    else if (opt == '/')
+    {
+        if (num2 == 0)
+        {
+            printf (" \n Divisor cannot be zero. Please enter another value ");
+            scanf ("%d", &num2);
+        }
+        res = num1 / num2;
+        printf ("%.2f", res);
+    }
+    else
+    {
+        printf(" \n You have entered wrong inputs ");
+    }
+    return 0;
+}
