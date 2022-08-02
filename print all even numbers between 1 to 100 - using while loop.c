7fd7d90e56507d7c87ee651f6f5c2f39a839209c
SchattenMonarch <110403431+SchattenMonarch@users.noreply.github.com>
2022-08-02 15:39:15 +0200

    Create print all even numbers between 1 to 100 - using while loop.c
    16.

---
+++ print all even numbers between 1 to 100 - using while loop.c
@@ -0,0 +1,11 @@
+//WAP program to print all even numbers between 1 to 100. - using while loop.
+#include <stdio.h>
+int main()
+{
+    int n = 2;
+    while(n<=100){
+        printf("%i\t", n);
+        n += 2;
+    }
+    return 0;
+}
