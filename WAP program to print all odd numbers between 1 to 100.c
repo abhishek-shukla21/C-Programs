c679b835e51cc4b3817579493f4bc5164b816238
SchattenMonarch <110403431+SchattenMonarch@users.noreply.github.com>
2022-08-02 15:46:12 +0200

    Create WAP program to print all odd numbers between 1 to 100.c
    17.

---
+++ WAP program to print all odd numbers between 1 to 100.c
@@ -0,0 +1,11 @@
+//WAP program to print all odd numbers between 1 to 100.
+#include <stdio.h>
+int main()
+{
+    int n = 1;
+    while(n<=100){
+        printf("%i\t", n);
+        n += 2;
+    }
+    return 0;
+}
