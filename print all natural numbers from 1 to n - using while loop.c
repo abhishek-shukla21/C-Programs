025b3287ec6cea5048202ce436299f603e9313b8
Dennis Folz <folzdennis@gmail.com>
2022-08-10 11:27:48 +0200

    added program description ... ... to print all natural numbers from 1 to n - using while loop.c

--- print all natural numbers from 1 to n - using while loop.c
+++ print all natural numbers from 1 to n - using while loop.c
@@ -1,4 +1,17 @@
-//WAP program to print all natural numbers from 1 to n. - using while loop.
+/*
+author: Dennis Folz
+github profile : https://github.com/SchattenMonarch
+date:   10.08.22
+
+program: WAP program to print all natural numbers from 1 to n - using while loop.
+
+Example 1
+
+Input
+    5
+Output
+	1	2	3	4	5
+*/
 #include <stdio.h>
 int main()
 {
