a72753bd28c5ed8cdb65144c36610357df9bc444
Dennis Folz <folzdennis@gmail.com>
2022-08-11 01:58:11 +0200

    Created the file find max & min element in an array.c Added the program description.

---
+++ find max & min element in an array.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+/*
+author: Dennis Folz
+github profile : https://github.com/SchattenMonarch
+date:   10.08.22
+
+program: WAP to find max & min element in an array.
+
+Example 1
+
+Input
+    4 9 5 80 10 2
+Output
+    min element: 2
+    max element: 80
+
+*/
