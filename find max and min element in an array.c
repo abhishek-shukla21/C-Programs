f41bd0f890c5f40e727d3105880476b5af161cf7
Dennis Folz <folzdennis@gmail.com>
2022-08-11 02:23:47 +0200

    Added useful #defines and a struct The struct contains an array of integers and an int for the size. Implemented createArray, which allocates memory for the array.

--- find max and min element in an array.c
+++ find max and min element in an array.c
@@ -14,3 +14,41 @@ Output
     max element: 80
 
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define TRUE  !0
+#define FALSE 0
+#define ARRAY_DEFAULT_SIZE ((int)20)
+#define RANDOM_ARRAY ((unsigned char)'a')
+#define ENTER_ELEMENTS ((unsigned char)'b')
+
+typedef struct{
+    int* values;
+    int size;
+}array;
+
+//clearStdinFromGarbage
+//clears stdin from anything. Wrong user inputs will be erased.
+void clearStdinFromGarbage(void) {
+    while (getchar() != '\n');
+}
+
+array* createArray(int size) {
+    if (size <= 0) 
+        return NULL;
+
+    array* arr = (array*)malloc(sizeof(array));
+    if (arr != NULL) {
+        arr->values = (int*)malloc(size * sizeof(int));
+        arr->size = size;
+        if (arr->values != NULL) {
+            for (int i = 0; i < size; i++) {
+                arr->values[i] = 0;
+            }
+            return arr;
+        }
+    }
+    return NULL;
+}
