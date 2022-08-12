a04fced257ee475d9258b0ab267409f67ad9275c
Dennis Folz <folzdennis@gmail.com>
2022-08-12 15:01:07 +0200

    Added the program description

---
+++ sort array in ascending order.c
@@ -0,0 +1,14 @@
+/*
+author: Dennis Folz
+github profile : https://github.com/SchattenMonarch
+date:   12.08.22
+
+program: WAP to sort array in ascending order
+
+Example 1
+
+Input
+    4 9 2 80 10 2
+Output
+    2 2 4 9 10 80
+*/
