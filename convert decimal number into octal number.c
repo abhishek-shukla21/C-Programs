666a120df459feeadad9167fdeb5863510ebaa10
Dennis Folz <folzdennis@gmail.com>
2022-08-14 20:35:33 +0200

    added program descrption and libraries

---
+++ convert decimal number into octal number.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+/*
+author: Dennis Folz
+github profile : https://github.com/SchattenMonarch
+date:   14.08.22
+
+program: WAP to convert Decimal number into Octal number
+Example 1
+
+Input
+    8
+Output
+    10
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
