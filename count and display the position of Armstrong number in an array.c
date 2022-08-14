6dec263df4d3ce37741e3953f1fbad513fd56dcc
Dennis Folz <folzdennis@gmail.com>
2022-08-14 21:48:04 +0200

    added the program description

---
+++ count and display the position of Armstrong number in an array.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+/*
+author: Dennis Folz
+github profile : https://github.com/SchattenMonarch
+date:   14.08.22
+
+program: WAP to count and display the position of Armstrong number in an array
+
+Example 
+
+Input
+    153 370 99 84 1634
+Output
+    found armstrong number at position#0: 153
+    found armstrong number at position#1: 370
+    found armstrong number at position#4: 1634
+*/
