38ea82118887304da7df7468e960507c1288e614
Dennis Folz <110403431+SchattenMonarch@users.noreply.github.com>
2022-08-16 22:04:58 +0200

    added a program description and author information
    ... to find sum of all even numbers between 1 to n.c

--- find sum of all even numbers between 1 to n.c
+++ find sum of all even numbers between 1 to n.c
@@ -1,4 +1,18 @@
-//WAP program to find sum of all even numbers between 1 to n.
+/*
+author: Dennis Folz
+github profile : https://github.com/SchattenMonarch
+date:   14.08.22
+
+program: WAP program to find sum of all even numbers between 1 to n.
+
+Example
+
+Input
+    5
+Output
+    15
+*/
+
 #include <stdio.h>
 int main()
 {
